b9247e0cab7157e02cee165fed122346ed3dfcef
Jeff Gitter <jgitter@nvisia.com>
2018-10-13 18:23:36 -0500

    Added the tracing micro-profile to chuck-service and gotten it working with the zipkin implementation.  Will expand to the front-end so we can see intertwined traces.

--- ChuckService/src/main/java/org/nvisia/openliberty/endpoint/ChuckController.java
+++ ChuckService/src/main/java/org/nvisia/openliberty/endpoint/ChuckController.java
@@ -4,16 +4,18 @@ import java.io.Serializable;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.opentracing.Traced;
 import org.nvisia.openliberty.dao.ChuckDao;
 import org.nvisia.openliberty.dao.icndb.ChuckJoke;
 import org.nvisia.openliberty.exceptions.BadRequestException;
+
+import io.opentracing.Tracer;
 
 @RequestScoped
 @Path("/chucks")
@@ -23,6 +25,9 @@ public class ChuckController implements Serializable {
 
 	@Inject
 	private ChuckDao dao;
+	
+	@Inject
+	private Tracer tracer;
 
 	/*
 	 * Oddly, constructor injection doesn't appear to work in CDI 2.0 on Liberty?
@@ -36,17 +41,23 @@ public class ChuckController implements Serializable {
 	@GET
 	@Path("")
 	@Produces(MediaType.APPLICATION_JSON)
+	@Traced
 	public ChuckJoke get() {
+		tracer.activeSpan().log("Getting random joke");
 		return dao.getRandomChuckJoke();
 	}
 
 	@GET
 	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public ChuckJoke get(@PathParam("id") Long id) {
+	@Traced
+	public ChuckJoke get(@PathParam("id") long id) {
 		if (id < 0) {
+			tracer.activeSpan().log(String.format("id value of %d is inavlid", id));
 			throw new BadRequestException("id must be a non-negative integer");
 		}
+		
+		tracer.activeSpan().log("Getting joke #" + id);
 		
 		return dao.getChuckJoke(id);
 	}
